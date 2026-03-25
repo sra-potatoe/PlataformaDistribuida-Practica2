4d104dba46ac5d588d66c01984a81f33267a3adf
Gabriel7589 <vaj4003681@est.univalle.edu>
2026-03-25 07:32:57 -0400

    BD_Fortaleza

---
+++ bancos/banco_10_fortaleza/Banco_Fortaleza_ejemplo.js
@@ -0,0 +1,29 @@
+db.cuentas.insertMany([
+    {
+        "Nro": 1,
+        "Identificacion": "45689234",
+        "Nombres": "Juan Carlos",
+        "Apellidos": "Pérez Gómez",
+        "NroCuenta": "FORT123456789012",
+        "IdBanco": 1,
+        "Saldo": 15250.7500
+    },
+    {
+        "Nro": 2,
+        "Identificacion": "78912345",
+        "Nombres": "María Elena",
+        "Apellidos": "López Fernández",
+        "NroCuenta": "FORT9876543210987654",
+        "IdBanco": 1,
+        "Saldo": 0.0009
+    },
+    {
+        "Nro": 3,
+        "Identificacion": "12345678",
+        "Nombres": "Carlos Andrés",
+        "Apellidos": "Mendoza Ríos",
+        "NroCuenta": "FORT4567891230456789",
+        "IdBanco": 1,
+        "Saldo": 3500.0000
+    }
+])
